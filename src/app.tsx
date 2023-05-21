b745e610efbd26f5d915d352c9b4c789e70d860e
ignxcy <ignacyy@proton.me>
2023-05-21 11:13:43 +0200

    app.tsx update

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,37 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
 
 export function App() {
   const [count, setCount] = useState(0)
+  const [countReset, setCountReset] = useState(0)
+
+  const incrementCount = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  const resetCount = () => {
+    setCount(0)
+  }
+
+  const decrementCount = () => {
+    if (count !== 0) {
+      setCount(prevCount => prevCount - 1)
+    }
+    else {
+      console.warn("Cannot go under zero.")
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+        <h1>{count}</h1>
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <button onClick={incrementCount}>+</button>
+        <button onClick={resetCount}>0</button>
+        <button onClick={decrementCount}>-</button>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
     </>
   )
 }

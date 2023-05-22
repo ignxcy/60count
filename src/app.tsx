554a3a205dfa1add6184b0831a1163fce8e20719
ignxcy <ignacyy@proton.me>
2023-05-22 07:44:35 +0200

    app.tsx && app.css updated

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,34 @@ import './app.css'
 
 export function App() {
   const [count, setCount] = useState(0)
+  let isLightMode = false;
+
+  const toggleMode = () => {
+    if (isLightMode) {
+      darkMode();
+      isLightMode = false;
+    } else {
+      lightMode();
+      isLightMode = true;
+    }
+  };
+  
+  const lightMode = () => {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+    // Additional styles for light mode
+    // ...
+  };
+  
+  const darkMode = () => {
+    document.body.style.backgroundColor = '#242424';
+    document.body.style.color = 'white';
+    // Additional styles for dark mode
+    // ...
+  };
+  
+  // Set the default mode to dark
+  darkMode();
 
   const incrementCount = () => {
     setCount(prevCount => prevCount + 1)
@@ -30,6 +58,10 @@ export function App() {
         <button onClick={incrementCount}>+</button>
         <button onClick={resetCount}>0</button>
         <button onClick={decrementCount}>-</button>
+      </div>
+      <div>
+        <label htmlFor="lightmode">light mode</label>
+        <input type="checkbox" id="lightmode" checked={isLightMode} onChange={toggleMode} />
       </div>
     </>
   )
